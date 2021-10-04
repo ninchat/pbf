59eb1b759cb70cd02d4f4e8e3448fc137de446ba
Timo Savola <timo@ninchat.com>
2021-10-04 04:37:19 +0300

    Fix lists in Go doc comments

--- bytecode.go
+++ bytecode.go
@@ -7,28 +7,28 @@ Bytecode
 
 Bytecode contains a 4-byte header and two sections:
 
-- Fields
-- Instructions and constants
+ - Fields
+ - Instructions and constants
 
 Field section:
 
-- Field count (1 byte)
-- Sequence of variable-length field specifications
+ - Field count (1 byte)
+ - Sequence of variable-length field specifications
 
 Field specification formats (NUM is protobuf field number or sequence index as
 fixed-width 32-bit integer, and SUBTYPE is protobuf field wire type):
 
-- NUM 0
-- NUM ModZigZag
-- NUM ModFloat
-- NUM ModPacked SUBTYPE ...
-- NUM ModMessage ...
-- NUM ModRepeated ...
+ - NUM 0
+ - NUM ModZigZag
+ - NUM ModFloat
+ - NUM ModPacked SUBTYPE ...
+ - NUM ModMessage ...
+ - NUM ModRepeated ...
 
 Instruction-and-constant section:
 
-- Sequence of variable-length instructions (opcodes followed by arguments)
-- Constant byte sequences are interleaved with the instructions
+ - Sequence of variable-length instructions (opcodes followed by arguments)
+ - Constant byte sequences are interleaved with the instructions
 
 The boundary between the sections is determined by the field count.
 
